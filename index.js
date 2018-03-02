1b3773d015d8f2b70ad83a8f00ab33c34631c469
ksha1018@naver.com <ksha1018@naver.com>
2018-03-02 11:47:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var recipes = {
+  breakfast: "tamato",
+  lunch: "rice"
+}
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object
+}
