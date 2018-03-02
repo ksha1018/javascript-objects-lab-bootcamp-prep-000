c6b1975ea37f89cd706275f516df9571f1c48510
ksha1018@naver.com <ksha1018@naver.com>
2018-03-02 11:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,17 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
+function deleteFromObjectByKey(object, key){
+  var ob1 = Object.assign({}, object);
+  delete ob1[key];
+  return ob1;
+}
+
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key];
   return object;
 }
+
+console.log(updateObjectWithKeyAndValue(recipes, "dinner", "chicken"));
+console.log(destructivelyUpdateObjectWithKeyAndValue(recipes, "dinner", "chicken"));
+
